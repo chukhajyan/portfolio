38b5fe3a9960c8b4661f69123e5ec70384cf7793
chukhajyan <levon.chukhajyan@gmail.com>
2018-08-09 16:51:15 +0400

    Minor Fixes

--- src/containers/Pages/Work/index.js
+++ src/containers/Pages/Work/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import WorkItem from '../../../components/workItem/';
 import barcampImage from '../../../images/barcamp.png';
 import globalRImage from '../../../images/globalr.png';
+import regnestImage from '../../../images/regnest.png';
+import planetSecurityImage from '../../../images/planet-security.jpeg';
 import './style.less';
 
 class Work extends Component {
@@ -12,8 +14,14 @@ class Work extends Component {
           <div className="work-portfolio">
             <div className="work-portfolio-1">
               <WorkItem backgroundImage={barcampImage} alt="Barcamp EVN" link="http://barcamp.am" technologyStackText="ReactJs, Redux, Sass"/>
-              <WorkItem backgroundImage={globalRImage} alt="GlobalR" link="http://globalr.com" technologyStackText="" id="globalrItem"/>
-              <WorkItem backgroundImage={barcampImage} alt="Barcamp EVN" link="http://barcamp.am" technologyStackText=""/>
+              <WorkItem backgroundImage={globalRImage} alt="GlobalR" link="https://globalr.com/" technologyStackText="ReactJs, Redux, Less, jQuery" id="globalrItem"/>
+              <WorkItem backgroundImage={planetSecurityImage} alt="Planet Security" link="http://cctvsmartsolution.com/" technologyStackText="Phoenix, Vanilla JS, jQuery"/>
+              
+            </div>
+            <div className="work-portfolio-1">
+             
+              <WorkItem backgroundImage={regnestImage} alt="Regnest" link="https://regnest.com/" technologyStackText="Vanilla JS, jQuery, Sass"/>
+              
             </div>
           </div>
         </div>
