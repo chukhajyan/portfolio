143cafecfab5243335b27b1be42155144b9c956a
Levon Chukhajyan <chukhajyan@Levons-MBP.Dlink>
2021-09-08 14:09:25 +0400

    Favicon and bug fixes

--- src/containers/Section_Two/Section_Two.js
+++ src/containers/Section_Two/Section_Two.js
@@ -5,7 +5,7 @@ function SectionTwo() {
   return (
     <div className="section-two-wrapper">
       {/* <Fade left cascade> */}
-        <p>I'm is a Product Manager with strong engineering and development background. He holds two bachelor's degrees: in Computer Science and Mining Engineering.</p>
+        <p>I'm is a Product Manager with strong engineering and development background. I hold two bachelor's degrees: in Computer Science and Mining Engineering.</p>
        {/* </Fade>s */}
       {/* <Fade left cascade> */}
         <p>I have experience of 4 years in developing mobile and web applications using the MERN stack. Deep knowledge and experience understanding of EVM, developing, deploying, and running dApps/Smart Contracts in production on Ethereum mainnet or L2/sidechain. I'm experienced in working with Javascript ethereum SDKs, ERC/EIP Ethereum standards, and smart contract development tools/frameworks.</p>
