88a346a197701938c1beb2ad9ec44b824e82e619
Levon Chukhajyan <chukhajyan@Levons-MBP.Dlink>
2021-09-08 15:09:00 +0400

    Section Three

--- src/containers/Section_Three/Section_Three.js
+++ src/containers/Section_Three/Section_Three.js
@@ -3,7 +3,34 @@ import './Section_Three.scss';
 function SectionTwo() {
   return (
     <div className="section-three-wrapper" id="projects">
-      
+      <h1>PROJECTS</h1>
+
+      <div className="projects-wrapper">
+        <div className="project-wrapper">
+          <h2>PROJECT NAME</h2>
+
+          <div className="details">
+            <p>Project detail down here</p>
+            <p>Project detail here</p>
+          </div>
+        </div>
+
+        <div className="project-wrapper">
+          
+        </div>
+
+        <div className="project-wrapper">
+          
+        </div>
+
+        <div className="project-wrapper">
+          
+        </div>
+
+        <div className="project-wrapper">
+          
+        </div>
+      </div>
     </div>
   );
 }
