f17131b6fc62f7466fac6a5f96e5333e1eb75da7
Levon Chukhajyan <levon.chukhajyan@gmail.com>
2021-09-09 16:21:15 +0400

    Projects Fixes

--- src/containers/Section_Two/Section_Two.js
+++ src/containers/Section_Two/Section_Two.js
@@ -5,7 +5,7 @@ function SectionTwo() {
     <div className="section-two-wrapper" id="about-me">
       <p>I'm is a Product Manager with strong engineering and development background. I hold two bachelor's degrees: in Computer Science and Mining Engineering.</p>
       <p>I have experience of 4 years in developing mobile and web applications using the MERN stack. Deep knowledge and experience understanding of EVM, developing, deploying, and running dApps/Smart Contracts in production on Ethereum mainnet or L2/sidechain. I'm experienced in working with Javascript ethereum SDKs, ERC/EIP Ethereum standards, and smart contract development tools/frameworks.</p>
-      <p>After finishing his mandatory military service in the Armed Forces of the Republic of Armenia, I'm fulfilling my passion for blockchain product development.</p>
+      <p>After finishing my mandatory military service in the Armed Forces of the Republic of Armenia, I'm fulfilling my passion for blockchain product development.</p>
     </div>
   );
 }
