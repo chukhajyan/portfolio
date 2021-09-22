f4a750028feefbad141a5e8a525a090831b3670e
Levon Chukhajyan <levon.chukhajyan@gmail.com>
2021-09-22 16:48:07 +0400

    Contact Me

--- src/containers/Section_Four/Section_Four.js
+++ src/containers/Section_Four/Section_Four.js
@@ -1,11 +1,24 @@
 import './Section_Four.scss';
+import { ReactComponent as LinkedIn } from '../../assets/social-icons/linkedin.svg';
+import { ReactComponent as Medium } from '../../assets/social-icons/medium.svg';
+import { ReactComponent as Github } from '../../assets/social-icons/github.svg';
+import { ReactComponent as Twitter } from '../../assets/social-icons/twitter.svg';
 
 function SectionFour() {
   return (
-    <div className="section-three-wrapper" id="contact-me">
+    <div id="contact-me">
       <h1>Contact Me</h1>
-      
-      
+
+      <div className="contact-me-wrapper">
+        <div className="social-icon-wrapper">
+          <a><LinkedIn className="social-icon" /></a>
+          <a><Medium className="social-icon" /></a>
+          <a><Github className="social-icon" /></a>
+          <a><Twitter className="social-icon" /></a>
+        </div>
+
+        <p>or <a className="email-link">e-mail me here</a></p>
+      </div>
     </div>
   );
 }
